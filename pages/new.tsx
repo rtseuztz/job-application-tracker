76563b719402cd8085c19e13b2c38a6116c65c5e
rtseuztz <yoyoguy111@gmail.com>
2022-08-14 03:26:19 -0500

    Adding functionality

--- pages/new.tsx
+++ pages/new.tsx
@@ -2,12 +2,59 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import newStyles from '../styles/New.module.css'
+import $ from 'jquery'
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+//import { getAnalytics } from "firebase/analytics";
+import { MouseEventHandler, useEffect, useRef } from 'react';
 
+import 'firebaseui/dist/firebaseui.css'
+import firebase from 'firebase/compat/app';
+import { initializeApp } from 'firebase/app';
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+const firebaseConfig = {
+  apiKey: "AIzaSyAJG719mWF0NIjf-YtVG2RqJV081jOqbs0",
+  authDomain: "social-media-manager-b449a.firebaseapp.com",
+  projectId: "social-media-manager-b449a",
+  storageBucket: "social-media-manager-b449a.appspot.com",
+  messagingSenderId: "606247064791",
+  appId: "1:606247064791:web:f5dc7ce2653e35354dcc92",
+  measurementId: "G-TGHNY65FH9"
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
 
 const Home: NextPage = () => {
+  const container=useRef(null);
+  useEffect( () => {
+    container.current = $(container.current)
+  }, [])
+  console.log(container);
+  const addUser: MouseEventHandler<HTMLButtonElement> = (e: any) => {
+    console.log(container);
+    if (!container)
+      return;
+    const selector = container?.current ?? $("main");
+    if(!selector) return;
+    const email = selector.find("#email")
+    const password = selector.find("#password")
+    console.log(e)
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+      });
+  }
   return (
     <div className={styles.container}>
       <Head>
@@ -15,26 +62,12 @@ const Home: NextPage = () => {
         <meta name="description" content="Manage you social media today" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          A smarter way to handle social media
-        </h1>
-
+      <main ref={container} className={styles.main}>
+        <h1 className={styles.title}>Welcome</h1>
+        <input id="email" className={newStyles.subtitle} placeholder="email"/>
+        <input id="password" className={newStyles.subtitle} placeholder="password"></input>
+        <button onClick={addUser}>Create</button>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   )
 }
